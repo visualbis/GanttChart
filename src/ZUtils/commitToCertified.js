51060bfb8234cd0c6c7db8382f970543f8480feb
sathish kumar <105407774+sathish151198@users.noreply.github.com>
2024-12-18 07:38:09 +0530

    chore(ver): certify GanttChart

--- src/ZUtils/commitToCertified.js
+++ src/ZUtils/commitToCertified.js
@@ -49,7 +49,7 @@ async function initVisualCodeCommit(componentName = '', buildType = '') {
     const CERTIFICATION_TARGET_BRANCH = 'C3';
     const BUILD_REPOS_ROOT = path.join(__dirname, './TEMP');
     const DEFAULT_FILES_PATH = path.join(__dirname, './defaultFiles');
-    const ROOT_PATH = path.resolve(__dirname, '../');
+    const ROOT_PATH = path.resolve(__dirname, '../../');
     const CONFIG_FOLDERS = ['.github'
     ];
     const FILES_TO_COPY = ['package.json',
